4a1d6fae76368cf0eccb11a54e4aa96cdf53fde9
kouzber123 <48252892+kouzber123@users.noreply.github.com>
2023-10-29 20:53:32 +0200

    Removed: allow credentials

--- API/Program.cs
+++ API/Program.cs
@@ -110,7 +110,7 @@ app.UseStaticFiles();
 
 app.UseCors(opt =>
 {
-    opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000");
+    opt.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
 });
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
